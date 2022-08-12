adde6a676350b3d5d6a591da08b461e0cae3e78e
Luciano Fastino <lucianofaustino10@gmail.com>
2022-08-12 08:15:03 -0300

    divisible sum pares

---
+++ Algorithms/Problem Solving/18-divisibleSumPairs.js
@@ -0,0 +1,20 @@
+function divisibleSumPairs(n, k, ar) {
+
+    let sum = 0;
+ 
+    for (let index = 0; index < n; index += 1) {
+       
+        for (let pair = 0; pair < n; pair += 1) {
+            
+            if (index != pair) {
+            
+                if ((ar[index] + ar[pair]) % k === 0) {
+                    sum += 1;
+                }
+            }
+        }
+    };
+    return sum / 2;
+}
+
+console.log(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2])); // 5
