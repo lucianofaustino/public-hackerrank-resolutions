aa526eab69bce779bcd2872526dec44388278aff
Luciano Fastino <lucianofaustino10@gmail.com>
2022-08-27 20:04:09 -0300

    add super reduced string

---
+++ Algorithms/Problem Solving/32-superReducedString.js
@@ -0,0 +1,19 @@
+function superReducedString(s) {
+
+    let arr = s.split('');
+    for (let index = 0; index < arr.length; index += 1) {
+        if (arr[index] === arr[index + 1]) {
+            arr.splice(index, 2);
+            index = -1;
+        }
+    }
+
+    if (arr.length === 0) {
+        return 'Empty String';
+    } else {
+        return arr.join('');
+    }
+
+}
+
+console.log(superReducedString('aaabccddd')); // abd
