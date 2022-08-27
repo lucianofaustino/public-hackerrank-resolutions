a87eee07626bb23c471206a6aee058773905aa7d
Luciano Fastino <lucianofaustino10@gmail.com>
2022-08-27 20:09:45 -0300

    add console.log no arquivo super reduced string

--- Algorithms/Problem Solving/32-superReducedString.js
+++ Algorithms/Problem Solving/32-superReducedString.js
@@ -1,9 +1,11 @@
 function superReducedString(s) {
 
     let arr = s.split('');
+    // console.log(arr) //[ 'a', 'a', 'a', 'b', 'c', 'c', 'd', 'd', 'd' ]
     for (let index = 0; index < arr.length; index += 1) {
         if (arr[index] === arr[index + 1]) {
             arr.splice(index, 2);
+            // console.log(arr.splice(index, 2)) // [ 'a', 'b' ], [ 'd', 'd' ]
             index = -1;
         }
     }
